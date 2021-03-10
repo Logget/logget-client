f84fa45b4acfb1278cd8e03fe7e923b14c242c43
miladd3 <milad.d3@gmail.com>
2021-03-10 06:53:43 +0330

    💅🏻 Add styles and SCSS structure

---
+++ src/layouts/NoLayout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface NoLayoutProps {
+
+}
+
+export const NoLayout: React.FC<NoLayoutProps> = ({}) => {
+    return <div className="no-layout"> </div>
+}
--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,22 +1,10 @@
 import React from "react";
-import "./App.css";
+import "../assets/styles/global.scss"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      wwww W
     </div>
   );
 }
