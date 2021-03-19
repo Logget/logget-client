b6e38abf0f9ca86ce8d5c0c2246eea13c76962ae
miladd3 <milad.d3@gmail.com>
2021-03-19 11:06:08 +0330

    💅🏻 Change style directories and configuration

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,15 +1,20 @@
 import React from "react";
-import "../assets/styles/global.scss";
-import { Route, BrowserRouter } from "react-router-dom";
+import "../styles/global.scss";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
 import { NoLayout } from "../layouts/NoLayout/NoLayout";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Route path="/">
-          <NoLayout>hello</NoLayout>
-        </Route>
+        <Switch>
+          <Route exact path="/">
+            <NoLayout>hello</NoLayout>
+          </Route>
+          <Route path="/login">
+            <NoLayout>login</NoLayout>
+          </Route>
+        </Switch>
       </BrowserRouter>
     </div>
   );
