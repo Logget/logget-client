bbe41f632cc3e0052871401557abd52b43629915
miladd3 <milad.d3@gmail.com>
2021-03-26 17:42:02 +0430

    🔘 add Button component

---
+++ src/components/forms/LtButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './LtButton.scss'
+interface LtButtonProps {
+  tag?: keyof JSX.IntrinsicElements
+  to?: string
+  type?: 'primary' | 'muted'
+}
+
+export const LtButton: React.FC<
+  LtButtonProps & React.HTMLProps<HTMLElement>
+> = (props) => {
+  const ButtonTag = (props.tag || 'button') as keyof JSX.IntrinsicElements
+
+  const className = `lt-btn${props.type ? ` lt-btn-${props.type}` : ''}`
+
+  if (props.to) {
+    return (
+      <Link className={className} to={props.to}>
+        {props.children}
+      </Link>
+    )
+  }
+
+  return (
+    <ButtonTag className={className} {...(props as any)}>
+      {props.children}
+    </ButtonTag>
+  )
+}
