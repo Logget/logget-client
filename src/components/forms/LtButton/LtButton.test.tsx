9711b0651adfd1ddc18316f8d61e4e4540506abc
miladd3 <milad.d3@gmail.com>
2021-04-01 21:01:01 +0430

    🧪 Testing click for LtButton

--- src/components/forms/LtButton/LtButton.test.tsx
+++ src/components/forms/LtButton/LtButton.test.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import React from 'react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { LtButton } from './LtButton'
-import { MemoryRouter } from "react-router-dom";
+import { MemoryRouter } from 'react-router-dom'
 
 
 test('LtButton: button should have role="button"', () => {
@@ -19,16 +19,32 @@ test('LtButton: children works ok', () => {
   expect(button).toHaveTextContent(text)
 })
 
-test('LtButton: Convert to Link when has "to" props',() => {
-  render(<MemoryRouter><LtButton to="/login">button</LtButton></MemoryRouter>)
+test('LtButton: Convert to Link when has "to" props', () => {
+  render(<MemoryRouter><LtButton to='/login'>button</LtButton></MemoryRouter>)
   const button = screen.getByRole('button')
 
   expect(button).toContainHTML('</a>')
 })
 
-test('LtButton: change tag based on prop',() => {
-  render(<LtButton tag="div" >button</LtButton>)
+test('LtButton: change tag based on prop', () => {
+  render(<LtButton tag='div'>button</LtButton>)
   const button = screen.getByRole('button')
 
   expect(button).toContainHTML('</div>')
+})
+
+
+test('LtButton: onClick testing', () => {
+  let x = 0
+
+  const addToX = () => {
+    x = 2
+  }
+
+  render(<LtButton tag='div' onClick={addToX}>button</LtButton>)
+  const button = screen.getByRole('button')
+
+  fireEvent.click(button)
+
+  expect(x).toBe(2)
 })
