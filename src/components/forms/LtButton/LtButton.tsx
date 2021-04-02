bd2f48c55497fb0c8d4ea3fa687bdf596b064fad
miladd3 <milad.d3@gmail.com>
2021-04-03 01:54:06 +0430

    🔘 Update LtButton

--- src/components/forms/LtButton/LtButton.tsx
+++ src/components/forms/LtButton/LtButton.tsx
@@ -1,29 +1,50 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './LtButton.scss'
+import remCalc from '../../../utils/remCalc'
+
 interface LtButtonProps {
   tag?: keyof JSX.IntrinsicElements
   to?: string
-  type?: 'primary' | 'muted'
+  type?: 'primary' | 'muted' | 'white'
+  height?: number
+  fontSize?: number
+  classes?: string
+  block?: boolean
 }
 
 export const LtButton: React.FC<
   LtButtonProps & React.HTMLProps<HTMLElement>
 > = (props) => {
+  const { height = 42, fontSize = 16, classes, block, type } = props
+
   const ButtonTag = (props.tag || 'button') as keyof JSX.IntrinsicElements
 
-  const className = `lt-btn${props.type ? ` lt-btn-${props.type}` : ''}`
+  const className = `lt-btn${type ? ` lt-btn-${type}` : ''}${
+    block ? ' lt-btn-block' : ''
+  } ${classes}`
+
+  const style = {
+    height: remCalc(height),
+    fontSize: remCalc(fontSize),
+    ...props.style,
+  }
 
   if (props.to) {
     return (
-      <Link className={className} to={props.to} role="button">
+      <Link className={className} style={style} to={props.to} role="button">
         {props.children}
       </Link>
     )
   }
 
   return (
-    <ButtonTag className={className} role="button" {...(props as any)}>
+    <ButtonTag
+      className={className}
+      style={style}
+      role="button"
+      {...(props as any)}
+    >
       {props.children}
     </ButtonTag>
   )
