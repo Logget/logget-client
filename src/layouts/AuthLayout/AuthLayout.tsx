154d5b6534708cc0ac2364e3eb5885fa2c72423e
miladd3 <milad.d3@gmail.com>
2021-04-03 01:53:10 +0430

    🛠 Add footer to AuthLayout

--- src/layouts/AuthLayout/AuthLayout.tsx
+++ src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { LogoType } from '../../components/shared/LogoType/LogoType'
 import './AuthLayout.scss'
 
-interface AuthLayoutProps {}
+interface AuthLayoutProps {
+  footer?: ReactNode
+}
 
-export const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
+export const AuthLayout: React.FC<AuthLayoutProps> = ({ children, footer }) => {
   return (
     <div className="auth-layout">
       <LogoType />
       <div className="circle" />
-      {children}
+
+      <main className="content">{children}</main>
+      <footer className="footer">{footer}</footer>
     </div>
   )
 }
