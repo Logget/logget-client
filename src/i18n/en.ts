dd799217af3081e3c77b6979de0b4f9a3a8428d0
miladd3 <milad.d3@gmail.com>
2021-04-16 05:47:25 +0430

    ✍️ Register Page Template

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -3,10 +3,17 @@ const en = {
   motto: "Time tracker You won't Forget to run",
   login: 'Login',
   getStarted: 'Get Started',
+  signUp: 'Sign Up',
   register: {
     createFree: 'Create a Free Logget account',
     googleLabel: 'Sign up via Google',
     emailLabel: 'Sign up with Email',
+    name: 'Name',
+    yourName: 'Your Name',
+    email: 'Email',
+    yourEmail: 'Your Email',
+    password: 'Password',
+    yourPassword: 'Your Password',
   },
 }
 
--- src/views/Register/Register.tsx
+++ src/views/Register/Register.tsx
@@ -1,8 +1,59 @@
 import React from 'react'
 import { AuthLayout } from '../../layouts/AuthLayout/AuthLayout'
+import { LtInput } from '../../components/forms/LtInput/LtInput'
+import { useTranslation } from 'react-i18next'
+import { LtButton } from '../../components/forms/LtButton/LtButton'
+import Icon from '@mdi/react'
+import { mdiArrowRight } from '@mdi/js'
+import './register.scss'
 
 interface RegisterProps {}
 
 export const Register: React.FC<RegisterProps> = () => {
-  return <AuthLayout className="register-email">register email</AuthLayout>
+  const { t } = useTranslation()
+
+  const Footer = (
+    <div className="register-footer">
+      <LtButton type="white" to="/login">
+        {t('login')}
+      </LtButton>
+      <LtButton type="primary">
+        {t('signUp')} <Icon className="icon" size={0.8} path={mdiArrowRight} />
+      </LtButton>
+    </div>
+  )
+
+  return (
+    <AuthLayout className="register-email" footer={Footer}>
+      <div className="input-group">
+        <label className="label" htmlFor="name">
+          {t('register.name')}
+        </label>
+        <LtInput placeholder={t('register.yourName')} id="name" />
+      </div>
+
+      <div className="input-group">
+        <label className="label" htmlFor="email">
+          {t('register.email')}
+        </label>
+        <LtInput
+          placeholder={t('register.yourEmail')}
+          id="email"
+          type="email"
+        />
+      </div>
+
+      <div className="input-group">
+        <label className="label" htmlFor="password">
+          {t('register.password')}
+        </label>
+        <LtInput
+          placeholder={t('register.yourPassword')}
+          id="password"
+          type="password"
+          name="password"
+        />
+      </div>
+    </AuthLayout>
+  )
 }
